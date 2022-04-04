4bc55656d88520e45d9332b49e943bb96fce20ae
miss-oso <Gladkikh.1307@yandex.ru>
2022-04-04 20:28:37 +0500

    Personal cost

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <h1>My personal costs</h1>
+     <Add-cost-module :maxCostID="maxCostID" @addCost="addCostToCosts()"/>
+    <CostsTable :costs="costs"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
+import CostsTable from './components/CostsTable.vue'
+import AddCostModule from './components/AddCostModule.vue'
+  
+  export default {
   name: 'App',
   components: {
-    HelloWorld
+    CostsTable,
+    AddCostModule
+  },
+
+data(){
+      return {
+        costs: [],
+        maxCostID: '',
+      }
+    },
+    methods: {
+      fetchData(){
+        return [
+          {
+            id: '1',
+            date: '28.03.2020',
+            category: 'Food',
+            value: '169'
+          },
+          {
+            id: '2',
+            date: '24.03.2020',
+            category: 'Transport',
+            value: '360'
+          },
+          {
+            id: '3',
+            date: '24.03.2020',
+            category: 'Food',
+            value: '532'
+          },
+        ]
+      },
+      getMaxId(){
+        this.maxCostID = this.costs[this.costs.length - 1].id
+      },
+      addCostToCosts(cost){
+        console.log(cost);
+        this.costs = [...this.costs, cost]
+      }
+    },
+    created() {
+      this.costs = [...this.fetchData()];
+      this.getMaxId();
+    }
   }
-}
 </script>
 
 <style>
